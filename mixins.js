44eb6324561a8b15c1ed911a488001977c031410
theroncross <cross.theron@gmail.com>
2015-03-05 09:41:42 -0800

    removed some files

--- mixins.js
+++
@@ -1,9 +0,0 @@
-function assignDefaults(defaults) {
-	for (var prop in this) {
-		if (this.hasOwnProperty(prop)) {
-			if (this[prop] === undefined) {
-				this[prop] = defaults[prop];
-			}
-		}
-	}
-}
